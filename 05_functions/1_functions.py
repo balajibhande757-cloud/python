db70ce887460593e139bbb405eac8f7214f6be51
balajibhande757-cloud <balajibhande757@gmail.com>
2026-06-22 11:36:00 +0000

    a

---
+++ 05_functions/1_functions.py
@@ -0,0 +1,16 @@
+# a=4
+# b=5
+# c=7
+# avrage=(a+b+c)/3
+# print(avrage)
+
+
+def avrage(a,b,c):
+    d=(a+b+c)/3.0
+    # print(d)
+    return d
+l1=avrage(4,5,7)
+l2=avrage(4,8,7)
+
+print(l1)
+print(l2)
